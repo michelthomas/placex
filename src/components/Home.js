992c9a2cd803b4744cbd4a2ced4b777cfeee70fc
Márcio Henrique <ikke.guerreiro@gmail.com>
2018-10-01 21:27:48 -0300

    -Recuperação e inserção das marcações do banco no mapa;

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,66 +1,70 @@
-import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
+import React, {Component} from 'react';
+import {Link} from 'react-router-dom';
 import firebase from 'firebase';
 
 export default class Home extends Component {
-  constructor(props) {
-    super(props);
+    constructor(props) {
+        super(props);
 
-    this.logout = this.logout.bind(this);
+        this.logout = this.logout.bind(this);
 
-    var currentUser = firebase.auth().currentUser;
-    if (!currentUser){
-      this.props.history.push('/');
+        const currentUser = firebase.auth().currentUser;
+        if (!currentUser) {
+            this.props.history.push('/');
+        }
     }
-  }
-  
-  logout(){
-    firebase.auth().signOut();
-    this.props.history.push('/');
-  }
 
-  addPlace(){
-    var place = {
-      description: 'Parque Munincipal',
-      price: 'gratuito',
-      openHours: '08:00 - 18:00',
-      likes: 624
-    };
+    logout() {
+        firebase.auth().signOut();
+        this.props.history.push('/');
+    }
 
-    firebase.database().ref('places').push(place)
-    .then(sucesso => {
-      console.log('Dados inseridos: ' + sucesso);
-    });
-  }
+    addPlace() {
+        const place = {
+            description: 'Parque Munincipal',
+            price: 'gratuito',
+            openHours: '08:00 - 18:00',
+            likes: 624
+        };
 
-  listPlaces(){
-    var ref = firebase.database().ref('places');
+        const db = firebase.firestore();
+        db.settings({
+            timestampsInSnapshots: true
+        });
+        const userRef = db.collection('Places').add(place)
+            .then(sucesso => {
+                console.log('Dados inseridos: ' + sucesso);
+            });
+    }
 
-    ref.once('value', function(snapshot) {
-      snapshot.forEach(function(childSnapshot) {
-        console.log(childSnapshot.val());
-      })
-    });
+    listPlaces() {
+        var ref = firebase.database().ref('places');
 
-  }
-  
-  render() {
-    return (
-      <div>
-        <h1>Home!</h1>
-        <div>
-            <Link to="/list">List Page</Link>
-        </div>
-        <div>
-            <Link to="/images">Images Page</Link>
-        </div>
-        <br/>
-        <a onClick={this.addPlace}>Cadastrar Local</a>
-        <br/>
-        <a onClick={this.listPlaces}>Listar Locais</a>
-        <br/>
-        <a onClick={this.logout}>Logout</a>
-      </div>
-    );
-  }
+        ref.once('value', function (snapshot) {
+            snapshot.forEach(function (childSnapshot) {
+                console.log(childSnapshot.val());
+            })
+        });
+
+    }
+
+    render() {
+        return (
+            <div>
+                <h1>Home!</h1>
+                <div>
+                    <Link to="/list">List Page</Link>
+                </div>
+                <div>
+                    <Link to="/images">Images Page</Link>
+                </div>
+                <br/>
+                <a onClick={this.addPlace}>Cadastrar Local</a>
+                <br/>
+                <a onClick={this.listPlaces}>Listar Locais</a>
+                <br/>
+                <a onClick={this.logout}>Logout</a>
+            </div>
+        );
+    }
 }
--- src/components/Map.js
+++ src/components/Map.js
@@ -1,52 +1,82 @@
-import React, { Component} from 'react';
+import React, {Component} from 'react';
 import GoogleMapReact from 'google-map-react';
 import Image from 'react';
-
 import marker from '../assets/baseline_place_black_18dp.png';
+import firebase from 'firebase';
 
 export default class Map extends Component {
-  static defaultProps = {
-      center: {lat: -9.665036, lng: -35.731161},
-      zoom: 15,
-      places: [{
-          lat: -9.664464,
-          lng: -35.732242
-      }, {
-          lat: -9.665634,
-          lng: -35.730389
-      }, {
-          lat: -9.665345,
-          lng: -35.735524
-      }]
-  };
+    constructor() {
+        super();
+        this.state = {
+            places: [],
+        }
+    }
+
+    componentDidMount() {
+        firebase.firestore().collection('Places').get().then((querySnapshot) => {
+            const places = [];
+            querySnapshot.forEach((doc) => {
+                    // doc.data() is never undefined for query doc snapshots
+                    console.log(doc.id, " => ", doc.data());
+
+                    const {categoria, coords, nome, descricao} = doc.data();
+
+                    places.push({
+                        key: doc.id,
+                        categoria: categoria,
+                        coords: coords,
+                        nome: nome,
+                        descricao: descricao,
+                    });
 
 
-  render() {
-    const style = {
-        width: '100vw',
-        height: '100vh'
+                }
+            );
+
+            this.setState({
+                places: places,
+            });
+        }).catch((error) => {
+            console.error(error);
+        });
+
+    }
+
+    static defaultProps = {
+        center: {lat: -9.665036, lng: -35.731161},
+        zoom: 15,
     };
-    return (
-        <div className='google-map' style={style}>
-            <GoogleMapReact
-                defaultCenter={this.props.center}
-                defaultZoom={ this.props.zoom }
-                bootstrapURLKeys={
-                    {key: 'AIzaSyDWc-bIxXW2k_6OEWmHw3Ybf4hHkNqCiBQ'}
-                }>
 
-                {this.props.places.map( (place) =>{
-                    return(
-                    <AnyReactComponent
-                        lat={place.lat}
-                        lng={place.lng}
-                        text={'Kreyser Avrora'}
-                    />
-                )})}
 
-            </GoogleMapReact>
-        </div>
-    );
-  }
+    render() {
+        const style = {
+            width: '100vw',
+            height: '100vh'
+        };
+        return (
+            <div className='google-map' style={style}>
+                <GoogleMapReact
+                    defaultCenter={this.props.center}
+                    defaultZoom={this.props.zoom}
+                    bootstrapURLKeys={
+                        {key: 'AIzaSyDWc-bIxXW2k_6OEWmHw3Ybf4hHkNqCiBQ'}
+                    }>
+
+                    {this.state.places.map((place) => {
+                        return (
+                            <AnyReactComponent
+                                lat={place.coords.lat}
+                                lng={place.coords.long}
+                                text={'Kreyser Avrora'}
+                                onChildClick={place.descricao}
+                                hover={place.desc}
+                            />
+                        )
+                    })}
+
+                </GoogleMapReact>
+            </div>
+        );
+    }
 }
-const AnyReactComponent = ({ text }) => <div><img src={marker}/></div>;
+const AnyReactComponent = ({text}) => <div><img src={marker}/></div>;
